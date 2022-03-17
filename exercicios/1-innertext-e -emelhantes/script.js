247ffb673495a1c3da4594538dde1c7caf95b12f
filippocamillo5 <filippocamillo5@icloud.com>
2022-03-17 15:07:12 -0300

    Initial commit

---
+++ exercicios/1-innertext-e -emelhantes/script.js
@@ -0,0 +1,8 @@
+let p = document.getElementById ("p1")
+
+p.textContent = "<h2> Tem um HTML dentro do parágrafo </h2>"
+
+p.innerText = "<h2> Tem um HTML dentro do parágrafo </h2>"
+p.innerHTML = "<h2> Tem um HTML dentro do parágrafo </h2>"
+
+p.style.backgroundColor = "green"
